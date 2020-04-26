c8ce5132d9114dbf23eec52aaf4acb0550a6728a
azahnen <zahnen@interactive-instruments.de>
2020-04-26 16:17:26 +0200

    netlify cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,6 @@ module.exports = ({ contentPath = "content" }) => ({
       },
     },
     `gatsby-plugin-netlify-cms`,
-    `gatsby-plugin-netlify-cache`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
